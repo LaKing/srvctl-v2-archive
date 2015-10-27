09cec2db58f40ee087ee6dcb182fb6eab4033cbe
codepad <codepad@srvctl.d250.hu>
2015-10-27 15:36:01 +0100

    2.2.3 - 2015.10.27-15:36:01

---
+++ template.sh
@@ -0,0 +1,21 @@
+## srvctl command template
+
+if $onVE && $isROOT
+then ## no identation.
+
+        hint "do-something [what]" "Dev an empty plugin! "
+        if [ "$CMD" == "do-something" ]
+        then
+            msg "Doing Something!"
+            log "Find out what to do."
+            err "Nothing to do!"
+            msg "Done with nothing!"
+        ok
+        fi ## do something
+
+fi
+
+man '
+    DO something! an empty plugin-like demo thingie for hisi.hr
+'
+
